8f93d5acad526aac40acc4e7460161bb1beb5865
Deepanshu9650 <gdeepanshu532@gmail.com>
2026-05-02 02:50:59 +0530

    Fix vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()],
+  server: {
+    allowedHosts: [
+      'surprising-nature-production-d28d.up.railway.app'
+    ]
+  }
 })
